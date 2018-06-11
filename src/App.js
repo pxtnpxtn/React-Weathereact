5716f178dca839c1992b2e1b49f75f01385e0f74
Paxton Eicher <paxton.w.eicher@gmail.com>
2018-06-11 18:11:01 -0400

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Title from "./components/Title";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <h1>Weathereact</h1>
+      <p>Find out the temperature, conditions and more...</p>
+    </div>
+  );
+};
+
+export default Title;
