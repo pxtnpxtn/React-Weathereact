2ff9ea8567b8d99e1f43f67a3dcfa25cc2b87e91
Paxton Eicher <paxton.w.eicher@gmail.com>
2018-06-11 19:58:35 -0400

    Add weather API fetch.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
 import "./App.css";
+
 import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "2e3b4817d0c852c41dacf29a6193c9a0";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  };
+
   render() {
     return (
       <div>
         <Title />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
     );
   }
